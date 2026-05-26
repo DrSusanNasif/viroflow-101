bf8a17f52f3bb6e9841b2f68f6efe92dde005d3f
Susan Nasif <susan.nasif.cimaza@gmail.com>
2026-05-26 19:11:52 +0200

    Count FASTA sequences

--- workflows/01-sequence-check/sequence_check.py
+++ workflows/01-sequence-check/sequence_check.py
@@ -7,5 +7,15 @@ print(f"Input file: {fasta_file}")
 
 if fasta_file.exists():
     print("Status: FASTA file found")
+
+    sequence_count = 0
+
+    with open(fasta_file, "r") as file:
+        for line in file:
+            if line.startswith(">"):
+                sequence_count = sequence_count + 1
+
+    print(f"Number of sequences: {sequence_count}")
+
 else:
     print("Status: FASTA file NOT found")
