916669a30cce20e0d85995f2ec0f97899fc17b84
Susan Nasif <susan.nasif.cimaza@gmail.com>
2026-05-26 19:16:05 +0200

    List FASTA sequence names

--- workflows/01-sequence-check/sequence_check.py
+++ workflows/01-sequence-check/sequence_check.py
@@ -9,13 +9,22 @@ if fasta_file.exists():
     print("Status: FASTA file found")
 
     sequence_count = 0
+    sequence_names = []
 
     with open(fasta_file, "r") as file:
         for line in file:
+            line = line.strip()
+
             if line.startswith(">"):
                 sequence_count = sequence_count + 1
+                sequence_name = line.replace(">", "")
+                sequence_names.append(sequence_name)
 
     print(f"Number of sequences: {sequence_count}")
+    print("Sequence names:")
+
+    for name in sequence_names:
+        print(f"- {name}")
 
 else:
     print("Status: FASTA file NOT found")
