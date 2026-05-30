4ff0e9f823cc4129573ead16e8bd5e8c444ac38f
Susan Nasif O-Adorisio <105450820+DrSusanNasif@users.noreply.github.com>
2026-05-30 10:27:19 +0000

    chore: clean up gitignore

--- work/71/2a1845ac93fa027d8d74a37b53b1be/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-python3 /workspaces/viroflow-101/bin/reverse_complement.py test_virus.fasta > reverse_complement.txt
--- work/f6/b8459e8d00382f74c09eec9d8cb8b4/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-python3 /workspaces/viroflow-101/bin/fasta_summary.py test_virus.fasta > genome_summary.txt
